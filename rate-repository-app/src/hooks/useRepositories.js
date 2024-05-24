92e8b191b38035d0a9b0a7065d707255490d4a46
adirajus <saiadi4002@gmail.com>
2024-05-24 16:20:39 +0100

    Added cache-and-network fetching policy to the query

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -2,7 +2,9 @@ import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from "../graphql/queries"
 
 const useRepositories = () => {
-  const { data, loading, refetch } = useQuery(GET_REPOSITORIES)
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network"
+  })
 
   if ( data ) {
     return { repositories: data.repositories, loading, refetch}
